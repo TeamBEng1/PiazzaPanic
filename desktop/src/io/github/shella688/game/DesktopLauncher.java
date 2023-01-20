74c2face30c4f37ce140d3e3ffae3656aed5ecc6
TeamBEng1 <123114047+TeamBEng1@users.noreply.github.com>
2023-01-20 11:02:17 +0000

    Add files via upload

---
+++ desktop/src/io/github/shella688/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package io.github.shella688.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import io.github.shella688.game.PiazzaPanic;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("PiazzaPanic");
+		new Lwjgl3Application(new PiazzaPanic(), config);
+	}
+}
